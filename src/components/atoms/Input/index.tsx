1ca7f4dfa29a17bc986acf62a33baedc88c962a8
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-09-29 13:55:12 +0700

    refactor(settings-page): create input form component

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,29 @@
+interface InputProps {
+  label: String;
+  type: String;
+  id: String;
+}
+
+export default function Input(props: InputProps) {
+  const { label, type, id, ...nativeProps } = props;
+
+  return (
+    <>
+      <label
+        htmlFor="name"
+        className="form-label text-lg fw-medium color-palette-1 mb-10"
+      >
+        {label}
+      </label>
+      <input
+        type={`${type}`}
+        className="form-control rounded-pill text-lg"
+        id={`${id}`}
+        name={`${id}`}
+        aria-describedby={`${id}`}
+        placeholder={`Enter your ${id}`}
+        {...nativeProps}
+      />
+    </>
+  );
+}
