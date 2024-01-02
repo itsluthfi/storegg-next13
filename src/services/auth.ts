7b189faf6c2d4e689b71772488d1a72ec8f3d027
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-01-02 15:09:21 +0700

    feat(services): create `setSignUp` function for auth services

---
+++ src/services/auth.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_URL = process.env.NEXT_PUBLIC_API;
+const API_VER = 'api/v1';
+
+export async function setSignUp(data) {
+  const URL = 'auth/signup';
+
+  const response = await axios.post(`${API_URL}/${API_VER}/${URL}`, data);
+  const axiosResponse = response.data;
+  return axiosResponse.data;
+}
+
+export async function setLogin() {
+  return null;
+}
