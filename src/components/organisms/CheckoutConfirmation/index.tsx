5ed9a92181183e27c4253e5d2f699db73426076c
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-01-11 17:33:25 +0700

    feat(checkout): set up checkout details data

--- src/components/organisms/CheckoutConfirmation/index.tsx
+++ src/components/organisms/CheckoutConfirmation/index.tsx
@@ -1,22 +1,51 @@
-import Link from 'next/link';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 export default function CheckoutConfirmation() {
+  const [checked, setChecked] = useState(false);
+
+  const onSubmit = () => {
+    const dataItemLocal = localStorage.getItem('data-item');
+    const dataTopUpLocal = localStorage.getItem('topup-data');
+
+    const dataItem = JSON.parse(dataItemLocal!);
+    const dataTopUp = JSON.parse(dataTopUpLocal!);
+
+    if (!checked) {
+      toast.warning('Pastikan kamu telah melakukan pembayaran!');
+    }
+
+    const data = {
+      voucher: dataItem._id,
+      nominal: dataTopUp.nominalItem._id,
+      payment: dataTopUp.paymentItem.payment._id,
+      bank: dataTopUp.paymentItem.bank._id,
+      name: dataTopUp.bankAccountName,
+      accountUser: dataTopUp.verifyID,
+    };
+
+    console.log(data);
+  };
+
   return (
     <>
       <label className="checkbox-label text-lg color-palette-1">
         I have transferred the money
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+        />
         <span className="checkmark" />
       </label>
       <div className="d-md-block d-flex flex-column w-100 pt-50">
-        <Link href="/complete-checkout" legacyBehavior>
-          <a
-            className="btn btn-confirm-payment rounded-pill fw-medium text-white border-0 text-lg"
-            role="button"
-          >
-            Confirm Payment
-          </a>
-        </Link>
+        <button
+          className="btn btn-confirm-payment rounded-pill fw-medium text-white border-0 text-lg"
+          type="button"
+          onClick={onSubmit}
+        >
+          Confirm Payment
+        </button>
       </div>
     </>
   );
