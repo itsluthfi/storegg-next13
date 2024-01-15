8bba36bbfa9b81150fb4a69ecd8becc56cb0cd9e
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-01-15 17:45:06 +0700

    feat(detail-page): add payments data for top-up form

--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -17,11 +17,13 @@ export default function Detail() {
     },
   });
   const [nominals, setNominals] = useState([]);
+  const [payments, setPayments] = useState([]);
 
   const getDetailVoucherAPI = useCallback(async (id) => {
     const data = await getDetailVoucher(id);
-    setDataItem(data);
-    setNominals(data.nominals);
+    setDataItem(data.detail);
+    setNominals(data.detail.nominals);
+    setPayments(data.payment);
     localStorage.setItem('data-item', JSON.stringify(data));
   }, []);
 
@@ -57,7 +59,7 @@ export default function Detail() {
             <div className="col-xl-9 col-lg-8 col-md-7 ps-md-25">
               <TopUpItem data={dataItem} type="desktop" />
               <hr />
-              <TopUpForm nominals={nominals} />
+              <TopUpForm nominals={nominals} payments={payments} />
             </div>
           </div>
         </div>
